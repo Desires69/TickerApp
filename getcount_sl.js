7facc216f7246ba548a998afe0dd2c81ff4f14c9
Mark Slade <markandrewslade@gmail.com>
2017-09-14 17:33:35 -0400

    Autoformat

--- getcount_sl.js
+++ getcount_sl.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var HttpsProxyAgent = require('https-proxy-agent'),
-    url = require('url'),
-    querystring = require('querystring'),
-    mongoose = require('mongoose'),
-    https = require('https'),
-    Ticker = require('./models/tickerModel');
+  url = require('url'),
+  querystring = require('querystring'),
+  mongoose = require('mongoose'),
+  https = require('https'),
+  Ticker = require('./models/tickerModel');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
@@ -22,31 +22,31 @@ default_start.setHours(default_start.getHours() - default_start_period);
 
 let prev_date = default_start;
 
-var count = 0;  
-var db = mongoose.connect('mongodb://localhost/tickerAPI',{useMongoClient: true}); 
-var update = function() {
+var count = 0;
+var db = mongoose.connect('mongodb://localhost/tickerAPI', {useMongoClient: true});
+var update = function () {
   Ticker.findOne().sort('-updated').exec((err, lastUpdate) => {
-    if(err) {
-        console.log(err);
+    if (err) {
+      console.log(err);
     } else {
-        //var lastUpdateJson = JSON.parse(lastUpdate);
-        if (lastUpdate) {
-          count = lastUpdate.count;
-          prev_date = lastUpdate.updated; 
-        }
+      //var lastUpdateJson = JSON.parse(lastUpdate);
+      if (lastUpdate) {
+        count = lastUpdate.count;
+        prev_date = lastUpdate.updated;
+      }
     }
 
     getToken();
   });
 }
 
-var tokenReceived = function(resp){
+var tokenReceived = function (resp) {
 
   console.log(resp);
   var resp_obj = JSON.parse(resp);
   var token = resp_obj.access_token;
   var type = resp_obj.token_type;
-  var cur_date = new Date(new Date().setHours(new Date().getHours()-24)).toISOString();
+  var cur_date = new Date(new Date().setHours(new Date().getHours() - 24)).toISOString();
   //var prev_date = new Date(new Date().setMinutes(new Date().getMinutes()-5)).toISOString();
   var oDataQuery = encodeURI("/utccloud.microsoft.com/activities/signinEvents?api-version=beta&$filter=signinDateTime ge " + prev_date + " and signinDateTime le " + cur_date + " and loginStatus eq '0'");
 
@@ -56,9 +56,9 @@ var tokenReceived = function(resp){
     port: '443',
     path: oDataQuery,
     headers: {
-	  'Authorization': type + ' ' + token,
+      'Authorization': type + ' ' + token,
       //'Proxy-Authorization': basicCredentials, 
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',           
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
     },
     Accept: '*/*',
     agent: proxyAgent
@@ -67,42 +67,41 @@ var tokenReceived = function(resp){
 
   function service_response_handler(resp) {
     var response = '';
-    resp.on('data', function(chunk) {
+    resp.on('data', function (chunk) {
       response += chunk;
     });
-    resp.on('end', function() {
-        service_response_received(response);
+    resp.on('end', function () {
+      service_response_received(response);
     });
-}
-function service_response_received(resp){
+  }
+  function service_response_received(resp) {
     try {
-        var service_resp_obj = JSON.parse(resp);
-    }
-    catch( e ) {
-        console.log(e);
+      var service_resp_obj = JSON.parse(resp);
+    } catch (e) {
+      console.log(e);
     }
     count += service_resp_obj.value.length;
-    if(service_resp_obj['@odata.nextLink']){
+    if (service_resp_obj['@odata.nextLink']) {
       var test = service_resp_obj['@odata.nextLink'];
-        req_options.path = url.parse(service_resp_obj['@odata.nextLink']).path;
-        service_request(req_options);
-    }else{
-        console.log(count);
-        var updateQuery = {'updated': cur_date, 'count': count}
-        var ticker = new Ticker(updateQuery);
-        console.log(ticker);
-        ticker.save();
+      req_options.path = url.parse(service_resp_obj['@odata.nextLink']).path;
+      service_request(req_options);
+    } else {
+      console.log(count);
+      var updateQuery = {'updated': cur_date, 'count': count}
+      var ticker = new Ticker(updateQuery);
+      console.log(ticker);
+      ticker.save();
     }
-}
-function service_request(options) {
+  }
+  function service_request(options) {
     console.log('requesting', options.path);
-  var serv_req = https.request(options, service_response_handler);
-  serv_req.end();
-}
+    var serv_req = https.request(options, service_response_handler);
+    serv_req.end();
+  }
 
 };
 
-var getToken = function(){
+var getToken = function () {
 
   var config = {
     client_id: '3111ac5a-c013-46b3-a5cb-d79081f9f2f1',
@@ -110,18 +109,18 @@ var getToken = function(){
     token_service: {
       host: 'login.microsoftonline.com',
       path: '/utccloud.onmicrosoft.com/oauth2/token?api-version=1.0'
-    } 
+    }
   };
 
-var token_input = {
+  var token_input = {
     grant_type: 'client_credentials',
     resource: '',
     client_id: config.client_id,
     client_secret: config.client_secret
   };
 
-var token_body = querystring.stringify(token_input);
-  
+  var token_body = querystring.stringify(token_input);
+
   var req_options = {
     method: 'POST',
     host: config.token_service.host,
@@ -131,7 +130,7 @@ var token_body = querystring.stringify(token_input);
       //'Proxy-Authorization': basicCredentials, 
       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(querystring.stringify(token_input))      
+      'Content-Length': Buffer.byteLength(querystring.stringify(token_input))
     },
     Accept: '*/*',
     agent: proxyAgent
@@ -139,22 +138,22 @@ var token_body = querystring.stringify(token_input);
 
   var token_body = querystring.stringify(token_input);
 
-  var req = https.request(req_options, function(res){
+  var req = https.request(req_options, function (res) {
 
     var response = '';
 
-    res.on('data', function(chunk) {
+    res.on('data', function (chunk) {
       response += chunk;
     });
 
-    res.on('end', function() {
+    res.on('end', function () {
       tokenReceived(response);
     });
-    
+
   });
-  
-  req.on('error', function(e) {
-       console.error(e);
+
+  req.on('error', function (e) {
+    console.error(e);
   });
 
   req.write(token_body);
