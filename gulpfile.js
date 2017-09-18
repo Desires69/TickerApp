d31fcc9625b7efb70d791e740947605ad37f864a
Bill Cahill <William.Cahill@hotmail.com>
2017-09-18 14:56:14 -0400

    added jscs to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 gulp.task('style', function(){
-    gulp.src(jsFiles)
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
-        }));
+        }))
+        .pipe(jscs());
 });
 
 gulp.task('default', function(){
